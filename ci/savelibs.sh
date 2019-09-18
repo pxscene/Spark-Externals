fe6002f5fa01eb72b959420b557ba45c3d439f4f
Madana Gopal <madanagopal_thirumalai@comcast.com>
2019-09-18 06:12:12 +0530

    Adding video components

--- ci/savelibs.sh
+++ ci/savelibs.sh
@@ -17,25 +17,36 @@ checkError()
 NODE_VER="10.15.3"
 
 #mention dirs for other externals directory
-NODE_LIB_DIR="`pwd`/libnode-v${NODE_VER}/out/Release/obj.target/"
+NODE_LIB_DIR="`pwd`/libnode-v${NODE_VER}/out/Release/obj.target"
 OPENSSL_LIB_DIR="`pwd`/openssl-1.0.2o/"
 SPARK_WEBGL_DIR="`pwd`/spark-webgl/build/Release/"
+EXT_LIBS_DIR=`pwd`/extlibs/lib
+EXT_INCLUDE_DIR=`pwd`/extlibs/include
 
 #copy to external directories
 EXT_INSTALL_PATH=$PWD/artifacts/${TRAVIS_OS_NAME}
+EXT_INSTALL_LIB_PATH=${EXT_INSTALL_PATH}/lib
+EXT_INSTALL_INCLUDE_PATH=${EXT_INSTALL_PATH}/include
+EXT_INSTALL_BIN_PATH=${EXT_INSTALL_PATH}/bin
+NODE_MODULES_PATH=${EXT_INSTALL_PATH}/node_modules
+
 if [ "$(uname)" != "Darwin" ]
 then
-  cp -R ${NODE_LIB_DIR}/libnode.so.64 ${EXT_INSTALL_PATH}/.
-  cp -R ${NODE_LIB_DIR}/../node ${EXT_INSTALL_PATH}/.
-  cp -R ${OPENSSL_LIB_DIR}/libcrypto.so.1.0.0 ${EXT_INSTALL_PATH}/.
-  cp -R ${OPENSSL_LIB_DIR}/libssl.so.1.0.0 ${EXT_INSTALL_PATH}/.
+  cp -R ${NODE_LIB_DIR}/libnode.so.64 ${EXT_INSTALL_LIB_PATH}/.
+  cp -R ${NODE_LIB_DIR}/../node ${EXT_INSTALL_BIN_PATH}/.
+  cp -R ${OPENSSL_LIB_DIR}/libcrypto.so.1.0.0 ${EXT_INSTALL_LIB_PATH}/.
+  cp -R ${OPENSSL_LIB_DIR}/libssl.so.1.0.0 ${EXT_INSTALL_LIB_PATH}/.
 else
-  cp -R ${NODE_LIB_DIR}/../libnode.*.dylib ${EXT_INSTALL_PATH}/.
-  cp -R ${NODE_LIB_DIR}/../node ${EXT_INSTALL_PATH}/.
-  cp -R ${OPENSSL_LIB_DIR}/libcrypto.*.dylib ${EXT_INSTALL_PATH}/.
-  cp -R ${OPENSSL_LIB_DIR}/libssl.*.dylib ${EXT_INSTALL_PATH}/.
+  cp -R ${NODE_LIB_DIR}/../libnode.*.dylib ${EXT_INSTALL_LIB_PATH}/.
+  cp -R ${NODE_LIB_DIR}/../node ${EXT_INSTALL_LIB_PATH}/.
+  cp -R ${OPENSSL_LIB_DIR}/libcrypto.*.dylib ${EXT_INSTALL_LIB_PATH}/.
+  cp -R ${OPENSSL_LIB_DIR}/libssl.*.dylib ${EXT_INSTALL_LIB_PATH}/.
 fi
-cp ${SPARK_WEBGL_DIR}/gles2.node ${EXT_INSTALL_PATH}/.
+cp ${SPARK_WEBGL_DIR}/gles2.node ${NODE_MODULES_PATH}/.
+
+#copy all externals dirs
+cp -R ${EXT_LIBS_DIR}/* ${EXT_INSTALL_LIB_PATH}/.
+cp -R ${EXT_INCLUDE_DIR}/* ${EXT_INSTALL_INCLUDE_PATH}/.
 
 #perform git commit
 git checkout $TRAVIS_BRANCH
@@ -44,5 +55,9 @@ REPO_NAME=`echo $TRAVIS_REPO_SLUG | cut -d'/' -f 2`
 git pull https://$REPO_USER_NAME:$GH_TOKEN@github.com/$REPO_USER_NAME/$REPO_NAME.git master
 git add ${EXT_INSTALL_PATH}
 git commit -m "update ${TRAVIS_OS_NAME} externals [skip ci]"
+if [ "$TRAVIS_PULL_REQUEST" = "true" ]
+then
+  exit 0;
+fi
 git push --repo="https://$REPO_USER_NAME:$GH_TOKEN@github.com/$REPO_USER_NAME/$REPO_NAME.git" 
 checkError $? "unable to commit data to repo" "" "check the credentials"
