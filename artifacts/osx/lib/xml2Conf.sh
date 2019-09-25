e8f8aa6e07803abbd8eefab0aded2aa8ad76ec0d
Travis CI <travis@Traviss-Mac-6.local>
2019-09-25 04:52:20 +0000

    update osx externals [skip ci]

---
+++ artifacts/osx/lib/xml2Conf.sh
@@ -0,0 +1,8 @@
+#
+# Configuration file for using the XML library in GNOME applications
+#
+XML2_LIBDIR="-L/Users/travis/build/kannanmadanagopalt/Spark-Externals/extlibs/lib"
+XML2_LIBS="-lxml2 -lz -L/usr/local/Cellar/xz/5.2.4/lib -llzma -lpthread  -liconv  -lm "
+XML2_INCLUDEDIR="-I/Users/travis/build/kannanmadanagopalt/Spark-Externals/extlibs/include/libxml2"
+MODULE_VERSION="xml2-2.9.4"
+
