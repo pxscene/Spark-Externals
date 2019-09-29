32545f2aaff2eb6fbaed41a169ba3288b0ff4e80
Travis CI <travis@Traviss-Mac-6.local>
2019-09-29 11:53:47 +0000

    update osx externals [skip ci]

---
+++ artifacts/osx/share/gdb/auto-load/Users/travis/build/pxscene/Spark-Externals/extlibs/lib/libgstreamer-1.0.so.0.1600.0-gdb.py
@@ -0,0 +1,10 @@
+import sys
+import gdb
+
+# Update module path.
+dir_ = '/Users/travis/build/pxscene/Spark-Externals/extlibs/share/gstreamer-1.0/gdb'
+if not dir_ in sys.path:
+    sys.path.insert(0, dir_)
+
+from gst_gdb import register
+register (gdb.current_objfile ())
