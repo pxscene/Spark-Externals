45f19100e6e66a7c4d51bc899b87a1d023c87812
Travis CI User <travis@example.org>
2019-09-29 10:18:57 +0000

    update linux externals [skip ci]

---
+++ artifacts/linux/lib/xml2Conf.sh
@@ -0,0 +1,8 @@
+#
+# Configuration file for using the XML library in GNOME applications
+#
+XML2_LIBDIR="-L/home/travis/build/pxscene/Spark-Externals/extlibs/lib"
+XML2_LIBS="-lxml2 -L/home/travis/build/pxscene/Spark-Externals/extlibs/lib -lz   -llzma      -lm "
+XML2_INCLUDEDIR="-I/home/travis/build/pxscene/Spark-Externals/extlibs/include/libxml2"
+MODULE_VERSION="xml2-2.9.4"
+
